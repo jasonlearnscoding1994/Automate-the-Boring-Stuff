b6d165bf19319c6a2c2ed12ea363545dc682c772
jasonlearnscoding1994 <jasonlearnscoding1994@users.noreply.github.com>
2018-03-08 01:19:26 +1100

    Sample code for player vs enemy
    Working code for encounter between player and enemy. Uses a randomly generated number to deal and receive damage for player and enemy.

--- sameName4.py
+++ sameName4.py
@@ -1,3 +1,9 @@
+# Example from Chapter 3, testing global variables.
+# Code tests the use of a local variable in a function before assigning a value to it.
+# The end result is an error because there is an assignment statement for eggs in spam()
+# and therefore considers eggs local to the function.
+
+
 def spam():
     print(eggs)
     eggs = 'spam local'
